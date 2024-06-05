bfaf2e28c468e7fc9d481dc1de22aae7af592ec0
Alexey <lexa8keis@gmail.com>
2024-06-05 16:13:24 +0900

    1 Задание

--- Homeworks/DZ2/Program.cs
+++ Homeworks/DZ2/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿// Задача 1!
+
+// System.Console.WriteLine("imput number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if(num % 7 == 0 && num % 23 == 0)
+// {
+//     System.Console.WriteLine("The number is a multiple of 7 and 23");
+// }
+// else
+// {
+//     System.Console.WriteLine("The number is not multiple of 7 and 23");
+// }
+
+// Задача 2!
+
