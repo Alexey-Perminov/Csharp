83c54499e051d2b6c94d66c8506358bf8a76f85b
Alexey <lexa8keis@gmail.com>
2024-06-05 15:33:23 +0900

    add

---
+++ Seminar/Sem1/Program.cs
@@ -0,0 +1,50 @@
+﻿// Console.WriteLine("Input number: ");
+
+// int x = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("quad of " + x + "is" + x * x);
+
+// Совместная работа
+// Семинар 1. Знакомство с языками программирования
+// Напишите программу, которая на вход
+// принимает два целых числа и проверяет,
+// является ли первое число квадратом второго.
+// a = 25, b = 5 => да
+// a = 2, b = 10 => нет
+// a = 9, b = -3 => да
+// a = -3, b = 9 => нет
+
+// Console.WriteLine("Input first number: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+
+// if(x == y * y)
+// {
+//     Console.WriteLine($"x = {x}, y = {y} -> yes");
+// }
+// else
+// {
+//     Console.WriteLine($"x = {x}, y = {y} -> no");
+// }
+
+// Console.WriteLine("Input n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int start = -n;
+// while(start <= n)
+// {
+//     System.Console.Write(start + " ");
+//     start ++;
+// }
+
+Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num >= 100 && num <= 999)
+{
+    int ed = num % 10;
+    int sot = num / 100;
+    System.Console.WriteLine("sum = " + (ed + sot));
+}
+else
+{
+    System.Console.WriteLine("You input not three-digit number");
+}
---
+++ Seminar/Sem2/Program.cs
@@ -0,0 +1,141 @@
+﻿// Методы-функции!!!
+// int Pow (int n)
+// {
+//     return n * n;
+// }
+
+// void Pow2(int n)
+// {
+//     System.Console.WriteLine($"Quad of {n} -> {n * n}");
+// }
+
+
+// System.Console.WriteLine("Input number: " );
+// int a = Convert.ToInt32(Console.ReadLine());
+// Pow2(a);
+
+// Напишите программу, которая принимает на
+// вход трёхзначное число и удаляет вторую
+// цифру этого числа.
+// Примеры
+// a = 256 => 26
+// a = 891 => 81
+
+
+// bool IsThreeDigit(int num)
+// {
+//     if (num > 99 && num < 1000)
+//     {
+//         return true;
+//     }
+//     return false;
+// }
+
+// int DeletSecondDigit(int num)
+// {
+//     if(IsThreeDigit(num))
+//     {
+//         int ed = num % 10;
+//         int sot = num / 100;
+//         return ed + sot * 10;
+//     }
+//     else 
+//     {
+//         System.Console.WriteLine("You imput not three-digit number");
+//         return 0;
+//     }
+// }
+
+
+
+
+// System.Console.WriteLine("Input number: " );
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine(DeletSecondDigit(a));
+
+// bool IsThreeDigit(int num)
+// {
+//     if (num > 99 && num < 1000)
+//     {
+//         return true;
+//     }
+//     return false;
+// }
+
+// void DeletSecondDigit(int num)
+// {
+//     if(IsThreeDigit(num))
+//     {
+//         int ed = num % 10;
+//         int sot = num / 100;
+//         System.Console.WriteLine(ed + sot * 10);
+//     }
+//     else 
+//     {
+//         System.Console.WriteLine("You imput not three-digit number");
+//     }
+// }
+// System.Console.WriteLine("Input number: " );
+// int a = Convert.ToInt32(Console.ReadLine());
+// DeletSecondDigit(a);
+
+// Напишите программу, которая принимает на вход
+// трёхзначное число и возводит вторую цифру этого
+// числа в степень, равную третьей цифре.
+// Примеры
+// 487 => 8^7 = 2 097 152
+// 254 => 5^4 = 625
+// 617 => 1
+
+// bool IsThreeDigit(int num)
+// {
+//     if (num > 99 && num < 1000)
+//     {
+//         return true;
+//     }
+//     return false;
+// }
+
+// void SecondToThirdPower(int num)
+// {
+//     if(IsThreeDigit(num))
+//     {
+//         int ed = num % 10;
+//         int dec = num / 10 % 10;
+//         System.Console.WriteLine(Math.Pow(dec, ed));
+//     }
+//     else 
+//     {
+//         System.Console.WriteLine("You imput not three-digit number");
+//     }
+// }
+
+// System.Console.WriteLine("Input number: " );
+// int a = Convert.ToInt32(Console.ReadLine());
+// SecondToThirdPower(a);
+
+// Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли первое число
+// кратным второму. Если первое число некратно
+// второму, то программа выводит остаток от деления.
+// Примеры
+// 14, 5 => нет, 4
+// 16, 8 => да
+// 4, 3 => нет, 1
+
+// void FirstDivSecond(int a, int b)
+// {
+//     if(a % b == 0)
+//     {
+//         System.Console.WriteLine($"{a} кратно {b}");
+//     }
+//     else 
+//     {
+//         System.Console.WriteLine($"{a} не кратно {b}");
+//     }
+// }
+// System.Console.WriteLine("Input number one: " );
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input number two: " );
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// FirstDivSecond(num1, num2);
