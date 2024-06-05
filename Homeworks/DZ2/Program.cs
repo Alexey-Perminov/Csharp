d1b8ef4a2dd5b33660d24ec0f2bc40fbd74f31a6
Alexey <Lexa8keis@gmail.com>
2024-06-05 16:18:00 +0900

    2 Задание

--- Homeworks/DZ2/Program.cs
+++ Homeworks/DZ2/Program.cs
@@ -14,3 +14,23 @@
 
 // Задача 2!
 
+// System.Console.WriteLine("Введите координаты точки x, y через пробел: ");
+// string[] coordinates = Console.ReadLine().Split(' ');
+// int x = Convert.ToInt32(coordinates[0]);
+// int y = Convert.ToInt32(coordinates[1]);
+// if (x == 0)
+//     System.Console.WriteLine("Точка находится на оси координат Y.");
+// if (y == 0)
+//     System.Console.WriteLine("Точка находится на оси координат X.");
+// else
+// {
+//     if (x > 0 && y > 0)
+//         System.Console.WriteLine("Точка находится в I четверти плоскости.");
+//     if (x > 0 && y < 0)
+//         System.Console.WriteLine("Точка находится в II четверти плоскости.");
+//     if (x < 0 && y < 0)
+//         System.Console.WriteLine("Точка находится в III четверти плоскости.");
+//     if (x < 0 && y > 0)
+//         System.Console.WriteLine("Точка находится в IV четверти плоскости.");
+// }
+
