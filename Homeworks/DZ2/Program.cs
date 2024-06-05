b6cd36c720ee32acd06b35e7e22bc90351d2617f
Alexey <Lexa8keis@gmail.com>
2024-06-05 16:30:24 +0900

    4 Задание

--- Homeworks/DZ2/Program.cs
+++ Homeworks/DZ2/Program.cs
@@ -43,3 +43,27 @@
 // int maxDigit = FirstDigit > SecondDigit ? FirstDigit : SecondDigit;
 //  Console.WriteLine(maxDigit);
 
+// Задача 4!
+
+Console.Write("Введите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 10)
+{
+    Console.WriteLine(N);
+}
+else
+{
+    while (N > 0)
+    {
+        int currentDigit = N % 10;
+        N /= 10;
+        if (N > 0)
+        {
+            Console.Write(currentDigit + ",");
+        }
+        else
+        {
+            Console.WriteLine(currentDigit);
+        }
+    }
+}
